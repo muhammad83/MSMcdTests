4ef8c703721e2d8151a3e34225ab4ed35c3dc2d4
DeFrenZ <defrenz@me.com>
2018-05-16 10:36:57 +0100

    add project

---
+++ BuddyBuildTest/BuddyBuildTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuddyBuildTest
+//
+//  Created by Davide De Franceschi on 16/05/2018.
+//  Copyright © 2018 Equal Experts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
