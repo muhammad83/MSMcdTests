56e334635085bbb3742caf7c3fa4a233593cdbb1
DeFrenZ <defrenz@me.com>
2018-05-18 16:07:10 +0100

    Use better placeholder for signaling version flow

--- BuddyBuildTest/scripts/reset_plist_versions.sh
+++ BuddyBuildTest/scripts/reset_plist_versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DEFAULT_VALUE="???"
+DEFAULT_VALUE="dynamically generated at buildtime"
 PlistBuddy=/usr/libexec/PlistBuddy
 INFOPLIST_PATH="${PROJECT_DIR}/${INFOPLIST_FILE}"
 $PlistBuddy -c "Set :CFBundleVersion $DEFAULT_VALUE" "$INFOPLIST_PATH"
